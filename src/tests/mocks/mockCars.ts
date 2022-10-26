f672929c06cf1fbea53f96dddc39abd88cb7b8e3
Fernando Rocha <fernandodasilvarocha2@gmail.com>
2022-10-26 20:50:40 -0300

    testes Model

---
+++ src/tests/mocks/mockCars.ts
@@ -0,0 +1,18 @@
+export const mockCars = {
+    model: 'Chevete Tuba',
+    year: 1963,
+    color: 'red',
+    buyValue: 3500,
+    seatsQty: 2,
+    doorsQty: 2
+}
+
+export const mockCarsId = {
+    model: 'Chevete Tuba',
+    year: 1963,
+    color: 'red',
+    buyValue: 3500,
+    seatsQty: 2,
+    doorsQty: 2,
+    _id: '6358a32d5d06ae79b5b74283',
+}
---
+++ src/tests/unit/models/carModel.test.ts
@@ -0,0 +1,49 @@
+
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import { ErrorTypes } from '../../../errors/catalog';
+import CarModel from '../../../models/Car';
+import { Model } from 'mongoose';
+import {
+	mockCars,
+	mockCarsId,
+} from '../../mocks/mockCars';
+
+
+describe('Frame Model', () => {
+	const carModel = new CarModel();
+
+	before(() => {
+		sinon.stub(Model, 'create').resolves(mockCarsId);
+		sinon.stub(Model, 'findOne').resolves(mockCarsId);
+	});
+
+	after(() => {
+		sinon.restore();
+	})
+
+	describe('creating a car', () => {
+		it('successfully created', async () => {
+			const newCar = await carModel.create(mockCars);
+			expect(newCar).to.be.deep.equal(mockCarsId);
+		});
+	});
+
+	describe('searching a car', () => {
+		it('successfully found', async () => {
+			const carFound = await carModel.readOne('62cf1fc6498565d94eba52cd');
+			expect(carFound).to.be.deep.equal(mockCarsId);
+		});
+
+		it('_id not found', async () => {
+			try {
+				await carModel.readOne('123ERRADO');
+			} catch (error: any) {
+				expect(error.message).to.be.eq(ErrorTypes.InvalidMongoId);
+			}
+		});
+	});
+
+	
+});
